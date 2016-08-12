85a9ad8421de84dc31a1aea753f46fe457bcd030
ToF- <cthibaut@octo.com>
2016-08-12 11:46:11 +0200

    calculating coverage and quality

--- DevEffort.hs
+++ DevEffort.hs
@@ -4,22 +4,30 @@ data DevSystem = DS { capacity  :: Double,
                       fixing    :: Double,
                       improving :: Double,
                       checking  :: Double,
-                      coverage  :: Double,
-                      quality   :: Double,
                       problems  :: Double,
                       additional:: Double,
-                      required  :: Double }
+                      required  :: Double,
+                      code      :: Double,
+                      checks    :: Double,
+                      improvements :: Double }
     deriving (Eq, Show)
 
 initial = DS { capacity   =  3.0,
                fixing     =  1.0,
                improving  =  1.0,
                checking   =  1.0,
-               coverage   =  0.0,
-               quality    =  0.0,
                problems   = 10.0,
                additional =  0.0,
-               required   =  1.0 }
+               required   =  1.0,
+               code       =  0.0,
+               checks     =  0.0,
+               improvements= 0.0 }
+
+coverage m | code m > 0.0 = checks m / code m 
+           | otherwise    = 0.0
+
+quality m | code m > 0.0 = improvements m / code m 
+          | otherwise    = 0.0
 
 capped m = (max 0) . (min m) 
 
@@ -38,3 +46,9 @@ improve_design t m = m { improving = v }
     where r = (capacity m - fixing m - checking m)
           v = capped r t
           
+evolve m = m { code   = (code m) + f,
+               checks = (checks m) + c,
+               improvements = (improvements m) + i }
+    where f = fixing m
+          c = checking m
+          i = improving m 
--- Specs.hs
+++ Specs.hs
@@ -3,7 +3,8 @@ import Test.QuickCheck
 import DevEffort
 
 shouldBe_d a e = (rounded a) `shouldBe` (rounded e)
-    where rounded = (/ 10000.0) . fromInteger . round . (* 10000.0)
+
+rounded = (/ 10000.0) . fromInteger . round . (* 10000.0)
 
 main = do 
     hspec $ do
@@ -27,7 +28,14 @@ main = do
                 it "additional problems" $ do
                     additional m `shouldBe_d` 0.0
                 it "required time per problem" $ do
-                    required m `shouldBe_d` 1.0 
+                    required m   `shouldBe_d` 1.0 
+                it "quantity of code" $ do
+                    code m       `shouldBe_d` 0.0 
+                it "quantity of checks" $ do
+                    checks m     `shouldBe_d` 0.0 
+                it "quantity of improvements" $ do
+                    improvements m `shouldBe_d` 0.0 
+
 
             describe "updating time spent on fixing problems" $ do
                 it "should not allow for negative amount" $ do
@@ -61,8 +69,37 @@ main = do
                 it "should be capped by capacity minus time spent fixing problems and adding checks" $ do
                     let m = improve_design 1 $ add_checks 1 $ fix_problems 1.8 initial
                     improving m `shouldBe_d` 0.2
+            describe "evolves" $ do
+                describe "code" $ do
+                    it "increases with time spent on fixing problems" $ do
+                        let m = evolve $ fix_problems  1 initial
+                        code m `shouldBe_d` 1.0
+                describe "checks" $ do
+                    it "increases with time spent on adding checks" $ do
+                        let m = evolve $ add_checks 1 $ fix_problems  1 initial
+                        checks m `shouldBe_d` 1.0
+                describe "improements" $ do
+                    it "increases with time spent on improving design" $ do
+                        let m = evolve $ improve_design 1 $ add_checks 1 $ fix_problems  1 initial
+                        improvements m `shouldBe_d` 1.0
+    
 
     quickCheck time_spent_is_capped_by_capacity
+    quickCheck coverage_equals_checks_on_code
+    quickCheck quality_equals_improvements_on_code
 
 time_spent_is_capped_by_capacity f d c = let m = improve_design d $ add_checks c $ fix_problems f initial
     in (fixing m + checking m + improving m) <= capacity m 
+
+coverage_equals_checks_on_code f d c = let d' = 1 + capped d 3.0
+                                           c' = 1 + capped c 3.0
+                                           f' = 1 + capped f 3.0
+                                           m = evolve $ improve_design d' $ add_checks c' $ fix_problems f' initial 
+    in rounded (coverage m) == rounded (checks m / code m) 
+ 
+quality_equals_improvements_on_code f d c = let d' = 1 + capped d 3.0
+                                                c' = 1 + capped c 3.0
+                                                f' = 1 + capped f 3.0
+                                                m = evolve $ improve_design d' $ add_checks c' $ fix_problems f' initial 
+    in rounded (quality m) == rounded (improvements m / code m) 
+ 
